58dbf1a3285a5dfe52754a68b311f4d90dab1964
Tejasv <145493365+Tejasv111@users.noreply.github.com>
2026-04-16 04:47:28 +0530

    final deployment prep2

--- api/index.js
+++ api/index.js
@@ -378,9 +378,9 @@ app.put("/api/board", async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
 
-});
+// });
 export default app;
 
